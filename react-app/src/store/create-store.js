fef48cbce34243c0aafa715a44b5c40a7df35a78
vvdementiev87 <91119943+vvdementiev87@users.noreply.github.com>
2022-02-10 19:55:02 +0300

    update#23

--- react-app/src/store/create-store.js
+++ react-app/src/store/create-store.js
@@ -6,6 +6,7 @@ import { messagesReducer } from "./messages";
 import { logger, timeScheduler, botMessage } from "./middlewares";
 import thunk from "redux-thunk";
 import storage from "redux-persist/lib/storage";
+import { getPublicGistsApi } from "../api/gists";
 
 const persistConfig = {
   key: "root",
@@ -23,7 +24,12 @@ export const persistedReduser = persistReducer(persistConfig, reducer);
 export const store = createStore(
   persistedReduser,
   compose(
-    applyMiddleware(logger, timeScheduler, botMessage, thunk),
+    applyMiddleware(
+      logger,
+      timeScheduler,
+      botMessage,
+      thunk.withExtraArgument({ getPublicGistsApi })
+    ),
     window.__REDUX_DEVTOOLS_EXTENSION__
       ? window.__REDUX_DEVTOOLS_EXTENSION__()
       : (args) => args
