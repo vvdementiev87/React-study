3be825dcc18e8f384c2cba2104ebd4812eb5b0d7
vvdementiev87 <91119943+vvdementiev87@users.noreply.github.com>
2022-02-12 10:41:51 +0300

    Revert "Merge branch 'homework#8' of https://github.com/vvdementiev87/React-study into homework#8"
    This reverts commit 3a56912221b6d73a390b4062be799545bfbbcddf, reversing
    changes made to 2d88143d22fac3b9ebd8e07410bab26b1e5b73bb.

--- react-app/src/pages/gists-page/gists-page.js
+++ react-app/src/pages/gists-page/gists-page.js
@@ -1,40 +1,46 @@
 import { useStyles } from "./use-style";
 import { useEffect, useState } from "react";
-import { getGists, gistsSelector } from "../../store/gists";
+import { getGists, searchGists, gistsSelector } from "../../store/gists";
 import { useDispatch, useSelector } from "react-redux";
+import debounce from "lodash.debounce";
+
+const searchGistsDebounced = debounce((query, dispatch) => {
+  dispatch(searchGists(query));
+}, 1000);
 
 export const GistsPage = () => {
   const styles = useStyles();
-  const [count, setCount] = useState("1");
-
+  const [count, setCount] = useState(1);
+  const [value, setValue] = useState("bogdanq");
   const dispatch = useDispatch();
-  const { pending, error } = useSelector(gistsSelector());
-  
-  console.log("Selector", useSelector(gistsSelector()));
-  console.log("Count",count );
+  const { gists, pending, error } = useSelector(gistsSelector());
+  const { gistsSearch, errorSearch, pendingSearch } = useSelector(
+    gistsSelector()
+  );
+
+  console.log("gistsSearch", gistsSearch);
+
   useEffect(() => {
-    
     dispatch(getGists(count));
   }, [dispatch, count]);
 
-  if (pending) {
+  useEffect(() => {
+    searchGistsDebounced(value, dispatch);
+  }, [dispatch, value]);
+
+  if (pending || pendingSearch) {
     return (
       <div className={styles.main}>
-        <p>Pending...</p>
-      </div>
-    );
-  }
-  if (error) {
-    return (
-      <div className={styles.main}>
-        <p>Error: {error}</p>
+        <p>pending....</p>
       </div>
     );
   }
 
   return (
-    
-      <div className={styles.main}>
+    <div className={styles.main}>
+      <div>{!!error && "Error"}</div>
+      <div>{!!errorSearch && "ErrorSearch"}</div>
+      <div>
         <button
           onClick={() => {
             setCount(count - 1);
@@ -49,7 +55,31 @@ export const GistsPage = () => {
           }}
         >
           next
-        </button>                
+        </button>
+
+        {gists?.map((gist, index) => (
+          <div key={index} className={styles.text}>
+            <p>{index + 1}</p>
+            <p>{gist.url}</p>
+            <p>{gist.description}</p>
+          </div>
+        ))}
       </div>
+      <div>
+        <input
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          placeholder="enter name"
+        />
+
+        {gistsSearch?.map((gist, index) => (
+          <div key={index} className={styles.text}>
+            <p>{index + 1}</p>
+            <p>{gist.url}</p>
+            <p>{gist.description}</p>
+          </div>
+        ))}
+      </div>
+    </div>
   );
 };
